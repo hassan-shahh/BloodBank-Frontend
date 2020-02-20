6eaf11b7973365e0e6f911a107174f77e9580016
hassan-shahh <hassanshah110@outlook.com>
2020-02-20 20:05:40 +0500

    first commit

---
+++ src/App12.js
@@ -0,0 +1,30 @@
+
+import React from 'react'
+import { render } from '@testing-library/react'
+class App12 extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 1
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    
+    handleClick() {
+        this.setState(previousState => {
+            return{
+                count : previousState.count + 1
+           }   })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
+            </div>
+        )
+    }
+}
+
+export default App12
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+//import App from './App';
+//import App3 from './App3';
+//import App2 from './App2';
+//import App5 from './App5';
+//import App6 from './App6';
+//import App4 from './App4';
+//import App7 from './App7'
+//import App8 from './App8'
+//import App from './App10'
+//import App11 from './App11'
+import App14 from './App14'
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App14/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
