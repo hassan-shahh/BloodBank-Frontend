e6c934282b4d79d47f69e8e1fe92bb3e60c89df4
hassan-shahh <61252610+hassan-shahh@users.noreply.github.com>
2020-04-24 08:04:52 +0500

    Update todoitem.js

--- src/Praactice/todoitem.js
+++ src/Praactice/todoitem.js
@@ -1,14 +1,51 @@
-import React from 'react'                   
-import  todata  from "./tododata"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
+class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.tots.completed ? "line-through" : "none"
+    };
+  };
 
-function Todoitem(props){
-    return(
-<div className= 'todoitem'>
-<input type="checkbox" checked={props.completed}/>
-    <p>{props.text}</p>
-    
-</div>
-    )
+  render() {
+    const { id, title } = this.props.tots;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />
+          {"    "}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            X
+          </button>
+        </p>
+      </div>
+    );
+  }
 }
-export default Todoitem
+
+TodoItem.propTypes = {
+  tots: PropTypes.object.isRequired
+};
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right"
+};
+
+const itemStyle = {
+  backgroundColor: "#f4f4f4"
+};
+export default TodoItem;
